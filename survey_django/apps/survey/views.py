514e8907f955db40645f90d3886b1e5167b8e404
vbamnup <vbamnup@gmail.com>
2023-03-24 12:59:01 +0300

    создание тестовых вьюшек

--- survey_django/apps/survey/views.py
+++ survey_django/apps/survey/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Главная страница")
+
+
+def survey_list(request):
+    return HttpResponse("Список опросов")
+
+
+def survey_detail(request, pk):
+    return HttpResponse(f"Опрос номер {pk}")
