1b61c83b4e4a3cc97491bcaf2d523fafee9c51c4
vbamnup <vbamnup@gmail.com>
2023-03-25 15:02:28 +0300

    create base models for survey and user

--- survey_django/apps/survey/admin.py
+++ survey_django/apps/survey/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Survey, Question, Option, Submission, Response, Answer
+
+
+@admin.register(Survey)
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ("title", "created_at")
+    list_filter = ("created_at",)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("survey", "prompt")
+    list_filter = ("survey",)
+
+
+@admin.register(Option)
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ("question", "text")
+
+
+@admin.register(Submission)
+class SubmissionAdmin(admin.ModelAdmin):
+    list_display = ("survey", "created_at")
+    list_filter = ("created_at",)
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ("user", "survey", "question", "answer")
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("submission", "option")
+    list_filter = (
+        "submission__survey",
+        "option",
+    )
